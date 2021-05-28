cb8bd795106206ac3b0daed50a758450b89ac3de
Martha30 <53914325+Martha30@users.noreply.github.com>
2021-05-28 14:17:40 -0600

    Update Actividades.js
    .

--- src/Actividades.js
+++ src/Actividades.js
@@ -1,11 +1,91 @@
-import React from 'react'
 
-function Actividades() {
-    return (
-        <div>
+import React, {Component}from 'react';
+import reciclar1 from "./reciclar1.jpg"
+import reciclar from "./reciclar.png"
+import macetas2 from "./macetas2.png"
+import macetas3 from "./macetas3.png"
+import arbol1 from "./arbol1.jpg"
+import huerto from "./huerto.png"
+import huerto4 from "./huerto4.png"
+import './Actividades.css';
+
+
+
+class Actividades extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            scrolled: true,
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', () => {
+            const isTop = window.scrollY < 50
+            if (isTop !== true){
+                this.setState({ scrolled: false });
+            }
+            else {
+                this.setState({ scrolled: true });
+            }
+        })
+    }
+    componentWillMount() {
+        window.removeEventListener('scroll', ()=>{});
+    }
+
+    render(){
+        return (
             
-        </div>
-    )
-}
+            <div className= 'header-text'>
+                ACTIVIDADES
+            <div className='titulo'>
+                Macetas utilizando botellas plásticas
+                <div className='titulo2'>
+                    Sembrar árboles
+                </div>
+                <div className='titulo3'>
+                    Elaboración de huertos
+                </div>
 
-export default Actividades
+            </div>
+                
+            <div className= 'macetas'>
+                <img src={reciclar1} width='300px' height='150px'/>
+
+                <div className= 'reciclar'>
+                    <img src={reciclar} width='300px' height='200px'/>
+                </div>
+                <div className= 'macetas2'>
+                    <img src={macetas2} width='300px' height='200px'/>
+                </div>
+                <div className= 'macetas3'>
+                    <img src={macetas3} width='300px' height='200px'/>
+                </div>
+
+            <div className='huertos'>
+            <div className='arbol1'>
+                <img src={arbol1} width='400px' height='250px'/>
+            
+                <div className='huerto2'>
+                    <img src={huerto} width='400px' height='250px'/>
+                </div>
+                <div className='huerto4'>
+                    <img src={huerto4} width='400px' height='250px'/>
+                </div>
+            
+            </div>  
+            </div>
+            </div>   
+             </div>
+
+        
+        
+        )
+
+    }
+}       
+
+
+export default Actividades;
