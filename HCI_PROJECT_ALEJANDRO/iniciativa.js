d34d11ce6d2a21b6544ef2457e1212d54ff70682
Alejandro Alvarez V <59927580+alejandroav93@users.noreply.github.com>
2021-05-27 22:45:49 -0600

    Iniciativa + Como Apoyar [HTML+CSS+JS]
    Iniciativa + Como Apoyar [HTML+CSS+JS]

---
+++ HCI_PROJECT_ALEJANDRO/iniciativa.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.principal');
+ScrollReveal().reveal('.cards', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.howto', { delay: 500 });
+ScrollReveal().reveal('.principal2', { delay: 500 });
