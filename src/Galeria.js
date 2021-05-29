9dda5125b14c0db4becc6d9f3449cc7c3f1c438d
Mirka <mirkanicolle@gmail.com>
2021-05-28 18:03:46 -0600

    galeria

--- src/Galeria.js
+++ src/Galeria.js
@@ -9,22 +9,22 @@ function Galeria() {
             </header>
             <div className="galeriageneral">
                 <div className="galeria1">
-                    <p className="subtitulogaleria">Ideas</p>
+                    <p className="subtitulogaleria">Cambio climatico</p>
                     <div className="imagen1"></div>
-                    <div className="imagen1"></div>
-                    <div className="imagen1"></div>
+                    <div className="imagen2"></div>
+                    <div className="imagen3"></div>
                 </div>
                 <div className="galeria1">
                     <p className="subtitulogaleria">Reciclaje</p>
-                    <div className="imagen1"></div>
-                    <div className="imagen1"></div>
-                    <div className="imagen1"></div>
+                    <div className="imagen4"></div>
+                    <div className="imagen5"></div>
+                    <div className="imagen6"></div>
                 </div>
                 <div className="galeria1">
-                    <p className="subtitulogaleria">Áreas verdes</p>
-                    <div className="imagen1"></div>
-                    <div className="imagen1"></div>
-                    <div className="imagen1"></div>
+                    <p className="subtitulogaleria">ODS</p>
+                    <div className="imagen7"></div>
+                    <div className="imagen8"></div>
+                    <div className="imagen9"></div>
                 </div>
             </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Actividades from './Actividades';
-import App from './App';
-import Expertos from './Expertos';
+import Galeria from './Galeria';
 
-ReactDOM.render(<Actividades/>, document.getElementById('root'));
+ReactDOM.render(<Galeria/>, document.getElementById('root'));
